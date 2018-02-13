778d4ea790c37320bd6ccc45b21715312be5652a
Charlie Gibbons <charlie.gibbons@brattle.com>
2018-02-13 09:18:10 -0800

    Fixed calculation

--- R/VarRWEstandard.R
+++ R/VarRWEstandard.R
@@ -2,9 +2,9 @@ VarRWEstandard <- function(model, K){
   if(is.null(model$model[["(weights)"]])){
     model$model[["(weights)"]] <- 1
   }
-  u <- model$residuals * model$model[["(weights)"]]
-  N <- nrow(u)
+  u <- model$residuals
+  N <- length(u)
   CORRECTION <- N/(N - K) # Small sample correction to match HC1 estimator
-  var <- CORRECTION * sum(u^2) /
-    sum((model$model[["(weights)"]]*model$model[[2]]^2))^2
+  var <- CORRECTION * sum(model$model[["(weights)"]]*u^2) /
+    sum((model$model[["(weights)"]]*model$model[[2]]^2)) / N
 }
